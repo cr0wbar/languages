25c6de820749ac00985f7fa5697236d54e9876d5
Guglielmo De Concini <guglielmo.deconcini@sensoworks.com>
2025-12-15 11:46:25 +0100

    Refactor fibonacci go code to mimic tail call optimisation

--- fibonacci/go/code.go
+++ fibonacci/go/code.go
@@ -1,23 +1,30 @@
 package main
+
 import (
-    "fmt"
-    "strconv"
-    "os"
+        "fmt"
+        "os"
+        "strconv"
 )
 
 func fibonacci(n int) int {
-  if n == 0 { return 0 }
-  if n == 1 { return 1 }
-  return fibonacci(n-1) + fibonacci(n-2)
+        if n == 0 {
+                return 0
+        }
+        a, b := 0, 1
+        for i := 2; i <= n; i++ {
+                a, b = b, a+b
+        }
+        return b
 }
 
 func main() {
-  input, e := strconv.Atoi(os.Args[1])
-  if e != nil { panic(e) }
-  u := int(input)
-  r := 0
-  for i := 0; i < u; i++ {
-    r += fibonacci(i)
-  }
-  fmt.Println(r)
+        input, e := strconv.Atoi(os.Args[1])
+        if e != nil {
+                panic(e)
+        }
+        r := 0
+        for i := 0; i < input; i++ {
+                r += fibonacci(i)
+        }
+        fmt.Println(r)
 }
